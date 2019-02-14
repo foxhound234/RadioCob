2cfb193cf1b66fbc05581dc707b4a956b179f6cc
foxhound234 <morganlb@protonmail.com>
2019-02-14 13:27:10 +0100

    service

--- application/views/Visiteur/Services.php
+++ application/views/Visiteur/Services.php
@@ -6,7 +6,7 @@
     <p>Inter quos Paulus eminebat notarius ortus in Hispania, glabro quidam sub vultu latens, odorandi vias periculorum occultas perquam sagax. is in Brittanniam missus ut militares quosdam perduceret ausos conspirasse Magnentio, cum reniti non possent, iussa licentius supergressus fluminis modo fortunis conplurium sese repentinus infudit et ferebatur per strages multiplices ac ruinas, vinculis membra ingenuorum adfligens et quosdam obterens manicis, crimina scilicet multa consarcinando a veritate longe discreta. 
         unde admissum est facinus impium, quod Constanti tempus 
         nota inusserat sempiterna.</p>
-
+        </div>
     <div class=bandeauPartenaire>
     <h1 class=infoloc> Publicité </h1>    
     </div>
